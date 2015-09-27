d88566075d2dc59e55315654b0060853c6327f10
Elliott Lawrence <elliottlawrence7@gmail.com>
2015-09-27 02:42:24 -0400

    Created FloProgram
    Created the FloProgram data type with methods to convert FloGraph ->
    FloProgram.

--- FloGraph.hs
+++ FloGraph.hs
@@ -1,6 +1,8 @@
 module FloGraph where
 
+import Data.List (find)
 import qualified Data.Map.Lazy as Map
+import Data.Maybe (fromJust)
 
 type Name = String
 
@@ -18,20 +20,22 @@ data Output = Output {
   getOutputType :: Type
 }
 
-{- A box is a generic representation of functions, constants, and literals. When
-   used in expressions, these appear as "black boxes", and only their interface
-   is visible. This consists of a name, a set of inputs (which is nonempty only
-   for functions), and an output. -}
+{- A box is a generic representation of functions, constructors, and literals.
+   When used in expressions, these appear as "black boxes", and only their
+   interface is visible. This consists of a name, a set of inputs, and an
+   output. -}
+data BoxFlavor = Function | Constructor | Literal
 data BoxInterface = BoxInterface {
+  getBoxFlavor :: BoxFlavor,
   getBoxName :: Name,
-  getInputs :: [Input],
-  getOutput :: Output
+  getBoxInputs :: [Input],
+  getBoxOutput :: Output
 }
 
 getBoxType :: BoxInterface -> Type
 getBoxType box = foldr1 (:->) (inputTypes ++ [outputType])
-  where inputTypes = map getInputType $ getInputs box
-        outputType = getOutputType $ getOutput box
+  where inputTypes = map getInputType $ getBoxInputs box
+        outputType = getOutputType $ getBoxOutput box
 
 {- Within a function definition, the same box can appear multiple times. Thus,
    boxes are identified by a unique key. -}
@@ -45,14 +49,33 @@ data Cable = ID :-: (ID, Input)
            | CableOut ID
 
 {- A box definition consists of the box's interface augmented with a set of
-   boxes and cables connecting nodes on the boxes. -}
+   boxes and cables connecting nodes on the boxes. In addition, a box may have
+   local box definitions, akin to a let/where clause in Haskell. -}
 data BoxDefinition = BoxDefinition {
   getBoxInterface :: BoxInterface,
   getBoxes :: BoxInterfaceMap,
-  getCables :: [Cable]
+  getCables :: [Cable],
+  getLocalDefinitions :: [BoxDefinition]
 }
 
-{- A module consists of a list of definitions. In turn, a program is simply a
-   list of modules. -}
+{- Get the box that is connected to a function's output. -}
+getOutputBox :: BoxDefinition -> ID
+getOutputBox boxDef = boxID
+  where cables = getCables boxDef
+        CableOut boxID = fromJust $ find isCableOut cables
+        isCableOut (CableOut _) = True
+        isCableOut _ = False
+
+{- Given a box definition and a specific box, find all of its inputs. -}
+getAppliedInputs :: BoxDefinition -> ID -> [(Input, ID)]
+getAppliedInputs boxDef boxID = concatMap filterCables cables
+  where cables = getCables boxDef
+        filterCables (inputID :-: (boxID', input))
+          | boxID' == boxID = [(input, inputID)]
+        filterCables (_ :-: _) = []
+        filterCables (CableOut _) = []
+
+{- A module consists of a list of definitions. In turn, a flo graph is a
+   graphical representation of a program, consisting of a list of modules. -}
 type Module = [BoxDefinition]
-type Program = [Module]
+type FloGraph = [Module]
---
+++ FloProgram.hs
@@ -0,0 +1,95 @@
+module FloProgram where
+
+import FloGraph
+
+import Control.Arrow (second)
+import qualified Data.Map.Lazy as Map
+import Data.Maybe (fromJust)
+import Text.Regex.Posix
+
+{- These are the primitive types that are supported by Flo. -}
+data Literal = LitInt Int | LitFloat Double | LitChar Char | LitString String
+
+{- Expressions can be literals, functions, constructors, applications, or let
+   expressions. -}
+data FloExpression = FloLit Literal                     -- Literal
+                   | FloFun Name Type                   -- Functions
+                   | FloCons Name Type                  -- Constructors
+                   | FloExpression :$: [(Input, FloExpression)]
+                                                        -- Applications
+                   | FloLet {                           -- Let expressions
+                     getLetDefinitions :: [FloDefinition],
+                     getLetExpression :: FloExpression
+                   }
+
+{- A function definition consists of a name, a list of inputs, and a defining
+   expression. -}
+data FloDefinition = FloDefinition {
+  getDefinitionName :: Name,
+  getInputs :: [Input],
+  getExpression :: FloExpression
+}
+
+{- A module consists of a list of definitions. In turn, a flo program is simply
+   a list of modules. -}
+type FloModule = [FloDefinition]
+type FloProgram = [FloModule]
+
+{- The following functions convert the graphical representation of a program
+   into a format composed of expressions and definitions. -}
+convertFloGraph :: FloGraph -> FloProgram
+convertFloGraph = map convertModule
+
+convertModule :: Module -> FloModule
+convertModule = map convertBoxDefinition
+
+{- The expression component of a box definition is the expression determined by
+   the output box. In addition, if a box has local definitions, these are
+   captured by wrapping the box definition in a let expression. -}
+convertBoxDefinition :: BoxDefinition -> FloDefinition
+convertBoxDefinition boxDef = FloDefinition {
+  getDefinitionName = getBoxName boxInterface,
+  getInputs = getBoxInputs boxInterface,
+  getExpression =
+    if null localDefinitions then boxExpression
+    else FloLet {
+      getLetDefinitions = map convertBoxDefinition localDefinitions,
+      getLetExpression = boxExpression
+    }
+}
+  where boxInterface = getBoxInterface boxDef
+        outputBoxID = getOutputBox boxDef
+        localDefinitions = getLocalDefinitions boxDef
+        boxExpression = createFloExpression boxDef outputBoxID
+
+createFloExpression :: BoxDefinition -> ID -> FloExpression
+createFloExpression boxDef boxID =
+  case boxFlavor of
+    Function -> createApplication boxDef boxID (FloFun boxName boxType)
+    Constructor -> createApplication boxDef boxID (FloCons boxName boxType)
+    Literal -> convertLiteral boxInterface
+  where boxInterface = fromJust $ Map.lookup boxID (getBoxes boxDef)
+        boxFlavor = getBoxFlavor boxInterface
+        boxName = getBoxName boxInterface
+        boxType = getBoxType boxInterface
+
+{- To create an expression out of a function or constructor, convert all of its
+   inputs to expressions and then apply them to the function. For constant
+   applicative forms, no inputs need be applied. -}
+createApplication :: BoxDefinition -> ID -> FloExpression -> FloExpression
+createApplication boxDef boxID leftSide
+  | null appliedInputs = leftSide
+  | otherwise = leftSide :$: appliedExpressions
+  where appliedInputs = getAppliedInputs boxDef boxID
+        appliedExpressions =
+          map (second $ createFloExpression boxDef) appliedInputs
+
+{- A literal is simply the box's name. -}
+convertLiteral :: BoxInterface -> FloExpression
+convertLiteral boxInterface = FloLit literal
+  where boxName = getBoxName boxInterface
+        literal | boxName =~ "\".*\"" = LitString (init $ tail boxName)
+                | boxName =~ "'.'" = LitChar (read boxName)
+                | boxName =~ "[:digit:]+" = LitInt (read boxName)
+                | boxName =~ "[:digit:]*\\.[:digit:]+" = LitFloat (read boxName)
+                | otherwise = error "Not valid literal"
